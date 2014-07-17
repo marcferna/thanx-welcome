ee8205f6f0dc3027e819262bdec5bd56a6808881
Marc Fernandez <marc.fernandezg@gmail.com>
2014-07-17 01:16:55 -0700

    Added default widget

---
+++ ios/StatsWidget/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  StatsWidget
+//
+//  Created by Marc Fernandez on 7/17/14.
+//  Copyright (c) 2014 Thanx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
